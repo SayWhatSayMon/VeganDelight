bbcb6aa16fb5db6e309a84db463f90f7813e6475
Simon Bühren <ge32toz@mytum.de>
2023-10-24 20:02:00 +0200

    Added soybean crop. First attempt...

--- src/main/java/de/chrisimo/vegandelight/item/ModItems.java
+++ src/main/java/de/chrisimo/vegandelight/item/ModItems.java
@@ -1,8 +1,10 @@
 package de.chrisimo.vegandelight.item;
 
 import de.chrisimo.vegandelight.VeganDelight;
+import de.chrisimo.vegandelight.block.ModBlocks;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,16 +15,32 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, VeganDelight.MODID);
 
     public static final RegistryObject<Item> TOFU = ITEMS.register("tofu",
-            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(5).saturationMod(0.4f).build())));
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    .nutrition(5)
+                    .saturationMod(0.4f)
+                    .build())));
 
     public static final RegistryObject<Item> SALT = ITEMS.register("salt",
-            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(5).saturationMod(0.4f).build())));
+            () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> MINCED_TOFU = ITEMS.register("minced_tofu",
-            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(5).saturationMod(0.4f).build())));
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    .nutrition(5)
+                    .saturationMod(0.4f)
+                    .build())));
 
     public static final RegistryObject<Item> COOKED_SMOKED_TOFU_SLICES = ITEMS.register("cooked_smoked_tofu_slices",
-            () -> new Item(new Item.Properties().food(new FoodProperties.Builder().nutrition(5).saturationMod(0.4f).build())));
+            () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    .nutrition(5)
+                    .saturationMod(0.4f)
+                    .build())));
+
+    public static final RegistryObject<Item> SOYBEAN = ITEMS.register("soybean",
+            () -> new ItemNameBlockItem(ModBlocks.SOYBEAN_CROP.get(),
+                    new Item.Properties().food(new FoodProperties.Builder()
+                            .nutrition(1)
+                            .saturationMod(0.1f)
+                            .build())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
