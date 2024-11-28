044afabc130c15d5c60aaa746b19455c43a63dc7
player005 <me@player005.net>
2024-11-28 17:42:16 +0100

    remove unused imports

--- src/main/java/de/chrisimo/vegandelight/fluid/ModFluidTypes.java
+++ src/main/java/de/chrisimo/vegandelight/fluid/ModFluidTypes.java
@@ -1,15 +1,11 @@
 package de.chrisimo.vegandelight.fluid;
 
 import de.chrisimo.vegandelight.VeganDelight;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraftforge.common.SoundAction;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import org.joml.Vector3f;
 
 public class ModFluidTypes {
     public static final DeferredRegister<FluidType> FLUID_TYPES =
