73700f32610d12143ac78005266a89a34379cc88
player005 <me@player005.net>
2025-01-01 16:26:47 +0100

    add more logging

--- common/src/main/java/net/player005/vegandelightfabric/recipe_manipulation/RecipeModification.java
+++ common/src/main/java/net/player005/vegandelightfabric/recipe_manipulation/RecipeModification.java
@@ -105,12 +105,13 @@ public abstract class RecipeModification {
     public static void init(RecipeManager recipeManager) {
         RecipeModification.recipeManager = recipeManager;
 
+        var timer = Stopwatch.createStarted();
         for (Consumer<RecipeManager> recipeManagerCallback : recipeManagerCallbacks) {
             recipeManagerCallback.accept(recipeManager);
         }
+        logger.debug("Executed {} recipe callbacks in {}", recipeManagerCallbacks.size(), timer);
 
-        var timer = Stopwatch.createStarted();
-        var modified = 0;
+        timer.reset().start();
 
         var byResultBuilder = ImmutableMultimap.<Item, RecipeHolder<?>>builder();
         for (RecipeHolder<?> recipeHolder : recipeManager.getRecipes()) {
@@ -118,6 +119,10 @@ public abstract class RecipeModification {
             byResultBuilder.put(result.getItem(), recipeHolder);
         }
         recipesByResult = byResultBuilder.build();
+        logger.debug("Built recipe by result map for {} recipes in {}", recipeManager.getRecipes().size(), timer);
+
+        timer.reset().start();
+        var modified = 0;
 
         for (RecipeHolder<?> recipeHolder : recipeManager.getRecipes()) {
             var registryAccess = recipeManager.registries;
