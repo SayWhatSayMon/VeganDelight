ed300dcc053ba3aa2c142c457511690da91a1132
SayWhatSayMon <saywhatsaymon@gmail.com>
2025-03-31 14:35:43 +0200

    added seitan with recipes and translation

--- src/main/java/net/player005/vegandelightfabric/VeganItems.java
+++ src/main/java/net/player005/vegandelightfabric/VeganItems.java
@@ -105,6 +105,18 @@ public class VeganItems {
                     .saturationMod(0.4f)
                     .build())));
 
+    public static final Item RAW_SEITAN = register("raw_seitan",
+            new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    .nutrition(2)
+                    .saturationMod(0.1f)
+                    .build())));
+
+    public static final Item COOKED_SEITAN = register("cooked_seitan",
+            new Item(new Item.Properties().food(new FoodProperties.Builder()
+                    .nutrition(2)
+                    .saturationMod(0.1f)
+                    .build())));
+
 
     // INGREDIENTS
     public static final Item SALT = register("salt",
@@ -149,9 +161,9 @@ public class VeganItems {
     public static ItemLike[] all = {
             VeganBlocks.SOYBEAN_BAG, VeganBlocks.WILD_SOYBEAN, SILKEN_TOFU, TOFU, TOFU_SLICES, COOKED_TOFU,
             COOKED_TOFU_SLICES, SMOKED_TOFU, SMOKED_TOFU_SLICES, COOKED_SMOKED_TOFU, COOKED_SMOKED_TOFU_SLICES,
-            MINCED_TOFU, TOFU_PATTY, TOFISH, COOKED_TOFISH, SMOKED_TOFISH, COOKED_SMOKED_TOFISH, TOFISH_ROLL,
-            SMOKED_TOFISH_ROLL, SOYMILK_BUCKET, SOYMILK_BOTTLE, SOYBEAN, SALT, APPLESAUCE, APPLESAUCE_BUCKET,
-            LEATHER_SUBSTITUTE
+            RAW_SEITAN, COOKED_SEITAN, MINCED_TOFU, TOFU_PATTY, TOFISH, COOKED_TOFISH, SMOKED_TOFISH,
+            COOKED_SMOKED_TOFISH, TOFISH_ROLL, SMOKED_TOFISH_ROLL, SOYMILK_BUCKET, SOYMILK_BOTTLE, SOYBEAN, SALT,
+            APPLESAUCE, APPLESAUCE_BUCKET, LEATHER_SUBSTITUTE
     };
 
 
