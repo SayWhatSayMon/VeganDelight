d4a1db352bb0bd3f4ba8797abc6858ac3ceab4e2
player005 <me@player005.net>
2025-04-07 17:17:08 +0200

    add a new ReplacementMap in order to show tooltips on replacement items

--- common/src/main/java/net/player005/vegandelightfabric/mixin/ItemStackMixin.java
+++ common/src/main/java/net/player005/vegandelightfabric/mixin/ItemStackMixin.java
@@ -6,7 +6,9 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.player005.vegandelightfabric.labels.LabelUtils;
+import net.player005.vegandelightfabric.labels.ReplacementMap;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,11 +16,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.List;
 
 @Mixin(ItemStack.class)
-public class ItemStackMixin {
+public abstract class ItemStackMixin {
+
+    @Shadow public abstract Item getItem();
 
     @Inject(method = "getTooltipLines", at = @At("RETURN"))
     private void injectLabels(Item.TooltipContext tooltipContext, Player player, TooltipFlag tooltipFlag,
                               CallbackInfoReturnable<List<Component>> cir) {
         LabelUtils.addTooltipLines((ItemStack) (Object) this, cir.getReturnValue());
+        ReplacementMap.addTooltipLines(getItem(), cir.getReturnValue());
     }
 }
