f615d757c2961ccfa9dafb5c5a7de483b768d08f
Player005 <me@player005.net>
2025-09-20 23:13:42 +0200

    change EntityMixin entrypoint

--- common/src/main/java/net/player005/vegandelightfabric/mixin/EntityMixin.java
+++ common/src/main/java/net/player005/vegandelightfabric/mixin/EntityMixin.java
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public abstract class EntityMixin {
 
-    @Inject(method = "spawnAtLocation(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("HEAD"))
-    private void markEntityDroppedItemsAsNotVegan(ItemStack stack, CallbackInfoReturnable<ItemEntity> cir) {
+    @Inject(method = "spawnAtLocation(Lnet/minecraft/world/item/ItemStack;F)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("HEAD"))
+    private void markEntityDroppedItemsAsNotVegan(ItemStack stack, float offsetY, CallbackInfoReturnable<ItemEntity> cir) {
         if (!((Object) this instanceof Player)) stack.applyComponents(VeganDataComponents.setIsNotVegan.get());
     }
 }
