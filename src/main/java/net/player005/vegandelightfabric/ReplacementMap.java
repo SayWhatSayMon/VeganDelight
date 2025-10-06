0384f2ef6f22f223fbdaa64162e8c9fd0424def3
Player005 <me@player005.net>
2025-10-06 23:57:54 +0200

    finish replacement tooltips

--- src/main/java/net/player005/vegandelightfabric/ReplacementMap.java
+++ src/main/java/net/player005/vegandelightfabric/ReplacementMap.java
@@ -1,28 +1,46 @@
 package net.player005.vegandelightfabric;
 
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Contains a map of which items are used to replace which.
  */
 public abstract class ReplacementMap {
 
-    private static final Map<Item, Item> map = new HashMap<>();
+    private static final Multimap<Item, Item> map = MultimapBuilder.hashKeys().arrayListValues().build();
 
     static {
         registerReplacement(VeganItems.LEATHER_SUBSTITUTE, Items.LEATHER);
         registerReplacement(VeganItems.SOYMILK_BUCKET, Items.MILK_BUCKET);
         registerReplacement(VeganItems.SOYMILK_BOTTLE, ModItems.MILK_BOTTLE.get());
+        registerReplacement(VeganItems.SILKEN_TOFU, Items.EGG);
+        registerReplacement(VeganItems.MINCED_TOFU, ModItems.MINCED_BEEF.get());
+        registerReplacement(VeganItems.TOFU_PATTY, ModItems.BEEF_PATTY.get());
+
+        registerReplacement(VeganItems.TOFISH, Items.COD);
+        registerReplacement(VeganItems.SMOKED_TOFISH, Items.SALMON);
+        registerReplacement(VeganItems.COOKED_TOFISH, Items.COOKED_COD);
+        registerReplacement(VeganItems.COOKED_SMOKED_TOFISH, Items.COOKED_SALMON);
+        registerReplacement(VeganItems.TOFISH_ROLL, ModItems.COD_ROLL.get());
+        registerReplacement(VeganItems.SMOKED_TOFISH_ROLL, ModItems.SALMON_ROLL.get());
+
+        registerReplacement(VeganItems.TOFU_SLICES, Items.MUTTON);
+        registerReplacement(VeganItems.TOFU_SLICES, Items.CHICKEN);
+        registerReplacement(VeganItems.SMOKED_TOFU_SLICES, Items.PORKCHOP);
+        registerReplacement(VeganItems.COOKED_TOFU_SLICES, Items.COOKED_MUTTON);
+        registerReplacement(VeganItems.COOKED_TOFU_SLICES, Items.COOKED_CHICKEN);
+        registerReplacement(VeganItems.COOKED_SMOKED_TOFU_SLICES, Items.COOKED_PORKCHOP);
     }
 
     /**
@@ -33,10 +51,10 @@ public abstract class ReplacementMap {
     }
 
     /**
-     * Returns the item that can be replaced using the given item, or null if the given item isn't registered as a
-     * replacement
+     * Returns the items that can be replaced using the given item,
+     * empty if the given item doesn't replace anything
      */
-    public static @Nullable Item replaces(Item item) {
+    public static Collection<Item> replaces(Item item) {
         return map.get(item);
     }
 
@@ -48,9 +66,18 @@ public abstract class ReplacementMap {
     @ApiStatus.Internal
     public static void addTooltipLines(Item item, List<Component> tooltip) {
         var replacedItem = replaces(item);
-        if (replacedItem == null) return;
+        if (replacedItem.isEmpty()) return;
+        var nameList = createItemList(replacedItem);
         tooltip.add(1, Component.translatable("tooltip.vegandelight.replaces",
-            Component.translatable(replacedItem.getDescriptionId()).setStyle(itemNameStyle)
+            nameList.setStyle(itemNameStyle)
         ).setStyle(textStyle));
+    }
+
+    private static MutableComponent createItemList(Collection<Item> items) {
+        var nameList = Component.empty();
+        var iter = items.iterator();
+        nameList.append(Component.translatable(iter.next().getDescriptionId()));
+        iter.forEachRemaining(it -> nameList.append(", ").append(Component.translatable(it.getDescriptionId())));
+        return nameList;
     }
 }
